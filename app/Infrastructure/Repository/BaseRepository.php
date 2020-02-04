034564ae0667c8524725edc948be4e6a5fee2396
Nima <nima.ebrazi1373@gmail.com>
2020-02-04 22:21:57 +0330

    Base repository created.
    Put all share method like find, fineOne, ... to it.

---
+++ app/Infrastructure/Repository/BaseRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Infrastructure\Repository;
+
+
+use App\Infrastructure\Repository\Contract\RepositoryInterface;
+
+class BaseRepository implements RepositoryInterface
+{
+
+}
