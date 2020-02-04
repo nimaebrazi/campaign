c183d2bedc3b2506d99c135182bed4d680d3f694
Nima <nima.ebrazi1373@gmail.com>
2020-02-04 22:58:59 +0330

    Register repositories in IoC.

--- app/Infrastructure/Repository/RepositoryServiceProvider.php
+++ app/Infrastructure/Repository/RepositoryServiceProvider.php
@@ -5,9 +5,12 @@ namespace App\Infrastructure\Repository;
 
 
 use App\Infrastructure\Repository\Cache\VoucherRepository as CacheVoucherRepository;
+use App\Infrastructure\Repository\Cache\VoucherUsageLogRepository as VoucherUsageLogCacheRepository;
 use App\Infrastructure\Repository\Contract\VoucherRepositoryInterface;
+use App\Infrastructure\Repository\Contract\VoucherUsageLogRepository;
 use App\Infrastructure\Repository\Eloquent\VoucherRepository as EloquentVoucherRepository;
 use Illuminate\Contracts\Cache\Repository as CacheRepository;
+use Illuminate\Contracts\Redis\Factory;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
@@ -25,12 +28,25 @@ class RepositoryServiceProvider extends ServiceProvider implements DeferrablePro
 
         });
 
+        $this->app->singleton(VoucherUsageLogRepository::class, function (Application $app) {
+            return new VoucherUsageLogCacheRepository(
+                $app->make(Factory::class)
+            );
+        });
+
+        $this->app->singleton(VoucherUsageLogCacheRepository::class, function (Application $app) {
+            return new VoucherUsageLogCacheRepository(
+                $app->make(Factory::class)
+            );
+        });
     }
 
     public function provides()
     {
         return [
             VoucherRepositoryInterface::class,
+            VoucherUsageLogRepository::class,
+            VoucherUsageLogCacheRepository::class,
         ];
     }
 }
